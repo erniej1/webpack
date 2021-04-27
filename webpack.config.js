abc84bdd5ef40931110b32cda8dcedcb091f082c
Ernie <ernestjohnson80@yahoo.com>
2021-04-27 11:33:47 -0700

    first

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const loader = require('sass-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+let mode = 'development';
+let target = 'web';
+
+if (process.env.NODE_ENV === 'producton') {
+  mode = 'production';
+  target = 'browserslist';
+}
+
+module.exports = {
+  mode: 'production',
+  target: target,
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' },
+          },
+
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
